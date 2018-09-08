3673441f0dc9ac5e3c0a62fd33cb8a355dd44065
amorgan <andrew.morgan@ing.nl>
2018-09-08 23:35:07 +0200

    Demo three answer

--- m2-stubbing/src/test/java/com/flights/Demo3AnswerTest.java
+++ m2-stubbing/src/test/java/com/flights/Demo3AnswerTest.java
@@ -15,7 +15,7 @@ import static com.flights.service.BookingResponse.BookingResponseStatus.SUCCESS;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class Demo2AnswerTest {
+public class Demo3AnswerTest {
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule();
@@ -35,8 +35,12 @@ public class Demo2AnswerTest {
                 "  \"paymentResponseStatus\": \"SUCCESS\"" +
                 "}")));
 
+        stubFor(get(urlPathEqualTo("/blacklisted-cards/1234-1234-1234-1234")).willReturn(okJson("{" +
+                "  \"blacklisted\": \"false\"" +
+                "}")));
+
         // When
-        final BookingResponse bookingResponse = bookingService.payForBooking("1111", "1234-1234-1234-1234", LocalDate.of(2018, 2, 1), new BigDecimal("20.55"));
+        final BookingResponse bookingResponse = bookingService.payForBookingWithFraudCheck("1111", "1234-1234-1234-1234", LocalDate.of(2018, 2, 1), new BigDecimal("20.55"));
 
         // Then
         assertThat(bookingResponse).isEqualTo(new BookingResponse("1111", "2222", SUCCESS));
